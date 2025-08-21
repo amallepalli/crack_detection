8c703c21c5d251e0e5fd90d00b50de2dc7108824
Luke Tuthill <lukeachu60@gmail.com>
2025-08-20 22:13:56 -0400

    3d projection, removal of duplicates working

---
+++ src/metashape.py
@@ -0,0 +1,126 @@
+import Metashape
+import os
+import json
+from shapely.geometry import Polygon
+
+json_folder = "C:\Programming\crack_detection\detection_json"
+layer_name = "Projected Damage"
+confidence_threshold = 0
+iou_threshold = .4
+
+def check_for_duplicate_masks(new_coords, polygons, iou_threshold):
+    new_polygon = Polygon(new_coords)
+    if not polygons:
+        polygons.append(new_polygon)
+        return False
+
+    for polygon in polygons:
+        intersection = new_polygon.intersection(polygon)
+        union = new_polygon.union(polygon)
+        if union.area == 0:
+            continue
+        iou_score = intersection.area / union.area
+        if (iou_score > iou_threshold):
+            return True
+        
+    polygons.insert(0, new_polygon)
+    return False
+
+def project_to_model(json_folder, layer_name, confidence_threshold=.2, iou_threshold=.7):
+    doc = Metashape.app.document
+    
+    chunk = doc.chunk
+    if not chunk:
+        print("No chunk found in the document")
+        return
+
+    if not chunk.shapes:
+        chunk.shapes = Metashape.Shapes()
+
+    surface = chunk.model
+
+    #chunk.crs = Metashape.app.getCoordinateSystem("Select Coordinate System", chunk.crs)
+    chunk.shapes.crs = chunk.crs
+    
+    matrix_t = chunk.transform.matrix
+    
+    # Check if group already exists
+    existing_group = None
+    for group in chunk.shapes.groups:
+        if group.label == layer_name:
+            existing_group = group
+            break
+    
+    if existing_group:
+        group = existing_group
+        print(f"Using existing group: {layer_name}")
+    else:
+        group = chunk.shapes.addGroup()
+        group.label = layer_name
+        group.color = (255, 0, 0)
+        print(f"Created new group: {layer_name}")
+
+    shapes_added = 0
+    polygons = [] #For checking duplicate detections
+    
+    for camera in chunk.cameras:
+        if not camera.transform:
+            print(f"Skipping {camera.label} - no transform")
+            continue
+            
+        json_file = os.path.join(json_folder, f"{camera.label}.json")
+
+        #If there are no detections, JSON file is not created and is skipped
+        if not os.path.exists(json_file):
+            continue
+
+        print(f"JSON file found: {json_file}")
+        try:
+            with open(json_file, 'r') as f:
+                detections = json.load(f)
+        except Exception as e:
+            print(f"Error reading {json_file}: {e}")
+            continue
+
+        for detection_type in ['crack_detections', 'spall_detections']:
+            for detection in detections.get(detection_type, []):
+                if 'segmentation_mask' in detection:
+                    try:
+                        # # Convert 2D image coordinates to 3D world coordinates
+                        # # Assuming points are in [x, y] format in image coordinates
+                        
+                        points_2d = detection['segmentation_mask']
+                        confidence = detection['confidence']
+                        if (confidence < confidence_threshold): continue
+                        projected_points = []
+
+                        for p in points_2d:
+                            point = surface.pickPoint(camera.center, camera.unproject(Metashape.Vector(p)))
+                            if point:
+                                projected_points.append(chunk.crs.project(matrix_t.mulp(point)))
+
+                        if len(projected_points) > 2:
+                            if (check_for_duplicate_masks([tuple(vec) for vec in projected_points], polygons, iou_threshold)):
+                                continue
+                            # if (len(polygons) > 50):
+                            #     polygons.pop()
+                            shape = chunk.shapes.addShape()
+                            shape.label = f"{detection_type.split('_')[0]} - {confidence:.2f}"
+                            shape.group = group
+                            shape.boundary_type = Metashape.Shape.BoundaryType.OuterBoundary
+                            shape.geometry = Metashape.Geometry.Polygon(projected_points)
+                            shapes_added += 1
+                        
+                    except Exception as e:
+                        print(f"Error creating shape for {camera.label}: {e}")
+
+    print(f"Added {shapes_added} shapes to group '{layer_name}'")
+    
+    if shapes_added > 0:
+        doc.save()
+        print("Document saved")
+    else:
+        print("No shapes were added")
+
+if __name__ == "__main__":
+    project_to_model(json_folder, layer_name, confidence_threshold, iou_threshold)
--- src/save_segmentation_results.py
+++ src/save_segmentation_results.py
@@ -1,3 +1,12 @@
+'''
+This code is to process a video and turn it into a 3d reconstruction.
+First step is to run the first function with the video you want to use.
+Then run the next function to process the images into json files.
+Then use those same images in Agisoft Metashape to create the 3d model of the structure.
+Make sure not to run both at the same time since they both use a lot of GPU resources.
+Once you have the 3d model and the json files, run the final function to combine them.
+'''
+
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
 import cv2
@@ -7,11 +16,20 @@ import os
 
 crack_model = YOLO("C:\Programming\crack_detection\models\crack_segmentation_model_02.pt")      
 spalling_model = YOLO("C:\Programming\crack_detection\models\spalling_segmentation_model_01.pt")
-video_path = r"C:\Programming\crack_detection\test_vid.mp4"
+video_path = r"C:\Programming\crack_detection\test_vid5.mp4"
 image_folder = "video_frames"
 json_folder = "detection_json"
-desired_fps = 5
+desired_fps = 2
 batch_size = 10
+
+def convert_types(obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    return obj
 
 def process_video_to_frames(video_path, image_folder, desired_fps):
     video = cv2.VideoCapture(video_path)
@@ -19,8 +37,8 @@ def process_video_to_frames(video_path, image_folder, desired_fps):
         print("Error, could not open video file")
         return
     
-    os.makedirs(image_folder, exist_ok=True)
-    os.makedirs(json_folder, exist_ok=True)
+    os.makedirs(image_folder, exist_ok=False) 
+    #If folder exists should throw exception, don't want to potentially delete files or have them in the same folder
 
     video_fps = video.get(cv2.CAP_PROP_FPS)
     frame_capture_interval = np.ceil(video_fps / desired_fps)
@@ -44,27 +62,35 @@ def process_video_to_frames(video_path, image_folder, desired_fps):
             output_path = os.path.join(image_folder, filename)
 
             cv2.imwrite(output_path, frame)
-            frames.append(output_path)
 
         frame_count += 1
 
     video.release()
     print(f"Successfully extracted {saved_frame_count} frames from the video")
-    return frames
 
-def process_images_to_json(crack_model, spalling_model, image_paths, json_folder, batch_size):
-    for i in range(0, len(image_paths) + batch_size, batch_size):
-        batch_paths = image_paths[i:min(i+batch_size, len(image_paths))]
+def process_images_to_json(crack_model, spalling_model, image_folder, json_folder, batch_size):
+    os.makedirs(json_folder, exist_ok=True)
+    try:
+        image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+        if not image_files:
+            print(f"No images found in '{image_folder}'. Please check the path.")
+            return
+    except FileNotFoundError:
+        print(f"Error: The directory '{image_folder}' was not found.")
+        return
+    
+    for i in range(0, len(image_files), batch_size):
+        batch_filenames = image_files[i:i+batch_size]
+        batch_paths = [os.path.join(image_folder, f) for f in batch_filenames]
 
         try:
             crack_results = crack_model(batch_paths, verbose=False)
             spall_results = spalling_model(batch_paths, verbose=False)
         except Exception as e:
-            print(f"Error processing {image_paths}: {e}")
+            print(f"Error processing {batch_paths}: {e}")
             return
 
-        for crack_result, spall_result, image_path in zip(crack_results, spall_results, batch_paths):
-            image_filename = os.basename(image_path)
+        for crack_result, spall_result, image_filename in zip(crack_results, spall_results, batch_filenames):
 
             output_data = {
                 "filename": image_filename,
@@ -72,11 +98,49 @@ def process_images_to_json(crack_model, spalling_model, image_paths, json_folder
                 "spall_detections": []
             }
 
-            class_names = result.names
+            if crack_result.boxes is not None:
+                for j in range(len(crack_result.boxes)):
+                    box = crack_result.boxes[j]
 
-            
+                    confidence = float(box.conf[0])
+                    
+                    detection_info = {
+                        "confidence": confidence,
+                        "bounding_box": box.xywhn[0].tolist(),
+                    }
 
+                    if crack_result.masks is not None and crack_result.masks.xy:
+                        detection_info["segmentation_mask"] = crack_result.masks.xy[j].tolist()
+                    
+                    output_data["crack_detections"].append(detection_info)
 
-if (__name__ == "__main__"):
-    image_paths = process_video_to_frames(video_path, image_folder, desired_fps)
-    #process_images_to_json(crack_model, spalling_model, image_paths, json_folder, batch_size)
+            if spall_result.boxes is not None:
+                for j in range(len(spall_result.boxes)):
+                    box = spall_result.boxes[j]
+
+                    confidence = float(box.conf[0])
+                    
+                    detection_info = {
+                        "confidence": confidence,
+                        "bounding_box": box.xywhn[0].tolist(),
+                    }
+
+                    if spall_result.masks is not None and spall_result.masks.xy:
+                        detection_info["segmentation_mask"] = spall_result.masks.xy[j].tolist()
+                    
+                    output_data["spall_detections"].append(detection_info)
+
+            if (output_data["crack_detections"] or output_data["spall_detections"]):
+                json_path = os.path.join(json_folder, f"{image_filename.split('.')[0]}.json")
+                try:
+                    with open(json_path, 'w') as f:
+                        json.dump(output_data, f, indent=4, default=convert_types)
+                except Exception as e:
+                    print(f"\nError writing JSON for {image_filename}: {e}")
+                print(f"{image_filename} has detections and the json file was successfully created")
+            else:
+                print(f"{image_filename} had no detections")
+
+if __name__ == "__main__":
+    process_video_to_frames(video_path, image_folder, desired_fps)
+    process_images_to_json(crack_model, spalling_model, image_folder, json_folder, batch_size)
